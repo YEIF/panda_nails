62e6ee7b6c0f22a468b0d71e20b44d4eb85fab49
IF <ast61006@gmail.com>
2022-03-25 08:29:07 +0800

    bootstrap scss

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bg-dark py-3 scb-footer position-relative">
+    <ul
+      class="text-white mb-md-0 mb-1 w-100 list-unstyled d-grid justify-content-center text-start"
+    >
+      <li class="mb-1">
+        <i class="bi bi-telephone-fill me-2"></i>
+        02-XXXX-XXX
+      </li>
+      <li class="mb-1">
+        <i class="bi bi-geo-alt-fill me-2"></i>
+        台北市西門町
+      </li>
+      <li class="mb-1">
+        <i class="bi bi-clock-fill me-2"></i>
+        營業時間 : 13:00 ~ 21:30
+      </li>
+      <li class="my-1 text-center">
+        <a
+          href="https://www.facebook.com/SugarCafeBar25588306"
+          target="_blank"
+          class="text-white mx-3"
+        >
+          <i class="bi bi-instagram h3"></i>
+        </a>
+      </li>
+      <li class="mb-0">© 2022 Panada Nails 版權所有(非商業用).</li>
+    </ul>
+  </div>
+</template>
